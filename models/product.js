49dd8539d2db78994fa7709c5219716ee8cf657d
Anurag Mittal <anuragmittal1598@gmail.com>
2020-07-25 14:45:31 +0530

    models 2nd commit

--- models/product.js
+++ models/product.js
@@ -2,5 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var product = new Schema({
-  
+  title: String,
+  price: Number,
+  likes: Number
 });
+
+module.exports = mongoose.model('Product',product);
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swag-shop');
+var product = require('./models/product');
+var wishlist = require('./models/wishlist');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));

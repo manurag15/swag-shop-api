82cc3936c8e093de7d7f98b25ab12d777c3121ea
Anurag Mittal <anuragmittal1598@gmail.com>
2020-07-26 16:43:27 +0530

    blah

--- models/product.js
+++ models/product.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 var product = new Schema({
   title: String,
   price: Number,
-  likes: {type:Number, default: 0}
+  likes: {
+    type: Number,
+    default: 0
+  }
 });
 
-module.exports = mongoose.model('Product',product);
+module.exports = mongoose.model('Product', product);
--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swag-shop');
-var product = require('./models/product');
+var Product = require('./models/product');
 var wishlist = require('./models/wishlist');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+// extended: false
+// }));
 
 
-app.post('/product', function(req, res){
-  var product = new product();
+app.post('/product', function(req, res) {
+
+  var product = new Product();
+  // res.send("Post request")
+
   product.title = req.body.title;
   product.price = req.body.price;
 
+  product.save(function(err, savedProduct) {
+    if (err) {
+      res.status(500).send({
+        error: "Could not save product"
+      });
+    } else {
+      res.status(200).send(savedProduct);
+    }
+
+  });
+
 });
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Swag Shop api running at port 3000");
 })
